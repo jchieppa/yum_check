68649f4b48127a4668c297f9a9f135e5666b8594
Jesse Chieppa <jesse@bookingcenter.com>
2014-01-07 19:52:09 -0800

    Initial commit. Made script more modular by moving domain into a variable.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-yum_check
-=========
+##########################
+# yum_check.pl script
+##########################
 
-Perl script to email a list of available updates from YUM
+INTRODUCTION
+============
+Perl script to run a yum list updates and email the contents to a defined email address
+
+INSTALLATION
+============
+Copy the yum_updates.pl script to /usr/local/sbin or other preferred directory.
+
+Add to your root user crontab a preferred interval.  We run ours every Monday so we have 
+the following in crontab
+
+* 2 * * 1 /usr/local/sbin/yum_check.pl > /dev/null 2>&1
+
+CONFIGURATION
+=============
+Edit the script to define variables relevant to your installation.  At minimum you need to 
+set the following variables:
+
+my $domain = "domain.com";
+my $to_addr = 'email@$domain';
+my $from_addr = "$host\@$domain";
+
+We check to make sure Yum isn't already running, If yum places it's pid file in a non 
+standard location you may need to update the following variable:
+
+my $pid = "/var/run/yum.pid";
---
+++ yum_check.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+####################################################################################
+# Run Yum Check-Update and email results to contact.
+# 1/7/2014
+# Version 1.0.3
+# jchieppa@gmail.com
+####################################################################################
+
+use strict;
+use warnings;
+use Sys::Hostname;
+use Mail::Sendmail;
+
+# MUST BE RUN AS ROOT
+my $login = (getpwuid $>);
+die "Must run as root.  Exiting!\n" if $login ne 'root';
+
+# DIE IF YUM ALREADY RUNNING
+my $pid = "/var/run/yum.pid";
+if (-e $pid) {
+	die "Yum already running.  Exiting!\n";
+ }
+ 
+# SCRIPT VARIABLES
+chomp (my $host = `hostname -s`);
+my $domain = "domain.com";
+my $to_addr = "email\@$domain";
+my $from_addr = "$host\@$domain";
+
+my @updates = `yum list updates -q`;
+
+my %mail = ( To     => $to_addr,
+            From    => $from_addr,
+            Subject => "Yum Available Updates for Host $host",
+            Message => "The following updates are currently available for $host\.$domain\n\n@updates"
+           );
+
+  sendmail(%mail) or die $Mail::Sendmail::error;
+  
+exit;
